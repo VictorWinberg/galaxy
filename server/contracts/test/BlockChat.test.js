df3a7ce258212b46e8ae60904191be3c7660d5c8
antonidag <antonidag@hotmail.com>
2022-01-26 16:59:22 +0100

    fix: unit test update

--- server/contracts/test/BlockChat.test.js
+++ server/contracts/test/BlockChat.test.js
@@ -11,10 +11,7 @@ contract("BlockChat", accounts => {
     })
   it("should be deployed", () =>{
     const address = token.address
-    assert.notEqual(address, 0x0)
-    assert.notEqual(address, '')
-    assert.notEqual(address, null)
-    assert.notEqual(address, undefined)
+    assert.ok(assert)
   })
   it("should send a message", async () =>{
     const sendMessage = await token.sendMessage('Hello Blockchain!', '1')
@@ -24,10 +21,9 @@ contract("BlockChat", accounts => {
     const sendMessage = await token.sendMessage('Well, hello there Mr.Chat!', '1')
     const getMessages = await token.getMessageByRoom('1')
     
-    
-    assert.notEqual(sendMessage, undefined)
-    assert.notEqual(getMessages, undefined)
+    assert.ok(sendMessage)
+    assert.ok(getMessages)
     assert.equal(getMessages[1].message,'Well, hello there Mr.Chat!')
-    assert.notEqual(getMessages.lenght,0)
+    assert.notEqual(getMessages.length,0)
   })
 });
