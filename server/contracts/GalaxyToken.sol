b86cb4fd4957aa3bdac8722ba4436a436401866e
Anton Björkman <antonidag@hotmail.com>
2022-02-07 21:43:32 +0100

    feat: adds abis for client (#14)
    Changes mintToken to check is Planet is claimed.
    Adds mapping of user planets and claimed planets.
    New abis
    * isClaimed
    * getUsersPlanets
    *getPlanet

--- server/contracts/GalaxyToken.sol
+++ server/contracts/GalaxyToken.sol
@@ -11,18 +11,64 @@ contract GalaxyToken is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
     address payable _owner;
+    struct Planet {
+        string Name;
+        address OwnerName;
+        uint timestamp;
+    }
+    mapping(uint256 => Planet) claimedPlanets;
+    mapping(address => uint256[]) userPlanets;
 
     constructor(address payable owner) ERC721("GalaxyToken", "NFT") {
         _owner = owner;
     }
 
-    function mintToken(address to) public payable virtual {
+    function mintToken(uint256 planetSeed, string calldata planetName, address to)
+        public
+        payable
+        virtual
+    {
+        require(!isClaimed(planetSeed), "Planet has already been claimed, nice try hacker, get lost.");
+        //Payment
         require(msg.value >= 10, "Not enough ETH sent; check price!");
         (bool sent, bytes memory data) = _owner.call{value: msg.value}("");
         require(sent, "Failed to send Ether");
 
+        // NFT 
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
         _mint(to, newItemId);
+        
+        // Internal
+        Planet memory p = Planet(planetName,msg.sender, block.timestamp);
+        claimedPlanets[planetSeed] = p;
+        userPlanets[msg.sender].push(planetSeed);
+    }
+
+    function isClaimed(uint256 planetSeed) public
+        view
+        returns (bool) {
+        Planet memory planet = claimedPlanets[planetSeed];
+        uint256 len = bytes(planet.Name).length;
+        if (len > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    function getUserPlanets(address userAddress)
+        public
+        view
+        returns (uint256[] memory)
+    {
+        return userPlanets[userAddress];
+    }
+
+    function getPlanet(uint256 planetSeed)
+        public
+        view
+        returns (Planet memory)
+    {
+        return claimedPlanets[planetSeed];
     }
 }
