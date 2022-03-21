2275437da97ab02a826d2717c9234208b4ce9401
Victor Winberg <victor.winberg@jayway.com>
2022-03-21 22:06:03 +0100

    app-router

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Galaxy from "./Galaxy";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/galaxy" element={<Galaxy />} />
+        <Route path="/galaxy/:location" element={<Galaxy />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
