f6be770137a72a9a5097c58aea94da38be698812
Anton Björkman <antonidag@hotmail.com>
2022-03-28 17:31:46 +0200

    add webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,13 +3,7 @@ module.exports = {
 
   resolve: {
     fallback: {
-    stream: require.resolve("stream-browserify"),
-    crypto: require.resolve("crypto-browserify"),
-      asset: require.resolve("assert"),
-      http: require.resolve("stream-http"),
-      https: require.resolve("https-browserify"),
-      url: require.resolve("url/"),
-      os: require.resolve("os-browserify/browser")
+       "url": false 
     }
   },
 }
