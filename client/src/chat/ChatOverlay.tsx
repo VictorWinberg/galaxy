eed131aaf2aa241415b6700dd5e379ab34e5d34c
Jan Zubac <jan.zubac@gmail.com>
2022-04-17 12:31:26 +0200

    Reintroduce styled components

--- client/src/chat/ChatOverlay.tsx
+++ client/src/chat/ChatOverlay.tsx
@@ -1,36 +1,66 @@
 import React, { useState } from "react";
+import styled, { css } from "styled-components";
 import Chat from "./Chat";
 import ChatIcon from "./ChatIcon";
 import CloseIcon from "./CloseIcon";
-import "./chatOverlay.css";
+
+const Wrapper = styled.div`
+  position: absolute;
+  width: 100%;
+  z-index: 100;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const InnerWrapper = styled.div<{ chatVisible: boolean }>`
+  display: flex;
+  ${({ chatVisible }) =>
+    chatVisible &&
+    css`
+      background-color: rgba(255, 240, 245, 0.3);
+      height: 100%;
+      width: 40%;
+    `}
+`;
+
+const ChatButton = styled.button`
+  padding: 0px;
+  border-radius: 50%;
+  height: 45px;
+  width: 45px;
+  border: none;
+  cursor: pointer;
+`;
+
+const CloseIconWrapper = styled.div`
+  position: absolute;
+  top: 8px;
+  right: 8px;
+  cursor: pointer;
+`;
 
 const ChatOverlay = () => {
   const [chatVisible, setChatVisible] = useState(false);
 
   return (
-    <div className="chat-overlay-wrapper">
-      <div
-        className={`chat-overlay-inner-wrapper ${
-          chatVisible ? "chat-visible" : ""
-        }`}
-      >
+    <Wrapper>
+      <InnerWrapper chatVisible={chatVisible}>
         {chatVisible ? (
           <>
-            <div
-              className="close-icon-wrapper"
-              onClick={() => setChatVisible(false)}
-            >
+            <CloseIconWrapper onClick={() => setChatVisible(false)}>
               <CloseIcon />
-            </div>
+            </CloseIconWrapper>
             <Chat />
           </>
         ) : (
-          <button className="chat-button" onClick={() => setChatVisible(true)}>
+          <ChatButton onClick={() => setChatVisible(true)}>
             <ChatIcon />
-          </button>
+          </ChatButton>
         )}
-      </div>
-    </div>
+      </InnerWrapper>
+    </Wrapper>
   );
 };
 
