a49f17296bebd923031f556d1ad82e5fc1f907ca
Anton Björkman <antonidag@hotmail.com>
2022-06-13 17:20:13 +0200

    refactor: remove web3 npm package

--- server/contracts/migration/3_galaxy_migration.js
+++
@@ -1,5 +0,0 @@
-const GalaxyToken = artifacts.require("GalaxyToken");
-
-module.exports = function (deployer) {
-  deployer.deploy(GalaxyToken, "{ownerAddress}"); //0xF41AD8b25F384c52d322bd073ec1c83A531e1Bc2
-};
