3f1693d06fe39e1e650f183a267becff971c1182
Victor Winberg <victor.winberg@doktor.se>
2022-06-28 18:11:06 +0200

    feat: use param location (#58)
    * feat: use param location
    
    * refactor: style with AI

--- client/src/ParamsPosition.ts
+++ client/src/ParamsPosition.ts
@@ -1,5 +1,6 @@
+import { useThree } from "@react-three/fiber";
 import { useEffect, useState } from "react";
-import { NavigateFunction } from "react-router-dom";
+import { Location, NavigateFunction } from "react-router-dom";
 
 export type Point3D = {
   x: number;
@@ -9,6 +10,7 @@ export type Point3D = {
 
 type Props = {
   navigate: NavigateFunction;
+  location: Location;
   position: Point3D | undefined;
   rotation: Point3D | undefined;
 };
@@ -16,8 +18,27 @@ type Props = {
 const round = (num: number, digits = 0) =>
   Math.round(num * Math.pow(10, digits)) / Math.pow(10, digits);
 
-export const useParamsPosition = ({ navigate, position, rotation }: Props) => {
+export const useParamsPosition = ({
+  navigate,
+  location,
+  position,
+  rotation,
+}: Props) => {
   const [timeout, setTimeout] = useState(-1);
+  const { camera } = useThree();
+
+  useEffect(() => {
+    const digit = "(-?\\d*\\.?\\d+)";
+    const exp = `${digit},${digit},${digit}`;
+    const regex = new RegExp(`@${exp}\\+${exp}`);
+
+    const matched = location.pathname.match(regex)?.map(Number);
+    if (matched) {
+      const [, x, y, z, rx, ry, rz] = matched;
+      camera.position.set(x, y, z);
+      camera.rotation.set(rx, ry, rz);
+    }
+  }, []);
 
   useEffect(() => {
     clearTimeout(timeout);
